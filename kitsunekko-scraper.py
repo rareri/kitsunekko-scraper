c7e1ebb8e1ac397704bc9adc2f86eae2ef090766
HeWhoIsLearningToCode <esparrag0@protonmail.com>
2022-10-23 14:55:12 -0700

    Update kitsunekko-scraper.py

--- kitsunekko-scraper.py
+++ kitsunekko-scraper.py
@@ -9,21 +9,19 @@ os.makedirs(anime, exist_ok=True)
 res = requests.get(anime_link)
 res.raise_for_status()
 soup = bs4.BeautifulSoup(res.content, 'lxml')
-links = soup.select('a[href]')
+links = soup.select('a')
 all_links = []
 for link in links:
-    link = str(link).split('"')
-    if len(link) > 3:
-        all_links.append(link[3])
+    link = link.get('href')
+    print(link)
+    if str(link).endswith(('.rar', '.7z', '.zip', '.ass', '.srt')):
+        all_links.append(link)
 
 for file in all_links:
     file_link = 'https://kitsunekko.net/' + file
     res = requests.get(file_link)
     res.raise_for_status()
     sub_file = open(os.path.join(anime, os.path.basename(file)),'wb')
-
-    #Nota: Escribir archivos en el diretorio de trabajo puede llegar a ocasionar errores
-    #      Si quieres probar el código hazlo en un directorio fijo
 
     for chunk in res.iter_content(100000):
         sub_file.write(chunk)
