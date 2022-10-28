44a4dda862cd3bcf0af0d79689180bd946860eca
HeWhoIsLearningToCode <esparrag0@protonmail.com>
2022-10-28 09:49:09 -0700

    Update kitsunekko-scraper.py

--- kitsunekko-scraper.py
+++ kitsunekko-scraper.py
@@ -4,7 +4,6 @@ base_link = 'https://kitsunekko.net/dirlist.php?dir=subtitles%2Fjapanese%2F'
 anime = sys.argv[1]
 anime_link = base_link + anime
 
-os.chdir(r'C:\Users\betit\Downloads')
 os.makedirs(anime, exist_ok=True)
 
 res = requests.get(anime_link)
@@ -14,19 +13,19 @@ links = soup.select('a')
 all_links = []
 for link in links:
     link = link.get('href')
-    print(link)
     if str(link).endswith(('.rar', '.7z', '.zip', '.ass', '.srt')):
         all_links.append(link)
+         
+        #Gets every the link for every file in the page
 
 for file in all_links:
     file_link = 'https://kitsunekko.net/' + file
     res = requests.get(file_link)
     res.raise_for_status()
     sub_file = open(os.path.join(anime, os.path.basename(file)),'wb')
-
-    #Nota: Escribir archivos en el diretorio de trabajo puede llegar a ocasionar errores
-    #      Si quieres probar el código hazlo en un directorio fijo
-
     for chunk in res.iter_content(100000):
         sub_file.write(chunk)
+
+        #Downloads every file in a folder named after the given input, and located in the current working directory
+ 
     sub_file.close()
